abbebe5f3ad5ef50f2beac49c11aa8a14c7dba52
David Wood <david@davidtw.co>
2016-05-30 15:01:12 +0100

    Now keeping track of the date and time added.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from . import db
 
 class Command(db.Model):
@@ -5,11 +6,13 @@ class Command(db.Model):
     command_name = db.Column(db.String(20), unique=True)
     help_text = db.Column(db.String(), nullable=False)
     response = db.Column(db.String(), nullable=False)
+    date_added = db.Column(db.DateTime(), nullable=False)
 
     def __init__(self, command_name, help_text, response):
         self.command_name = command_name
         self.help_text = help_text
         self.response = response
+        self.date_added = datetime.now()
 
     def __repr__(self):
         return '<Command %r>' % self.command_name
