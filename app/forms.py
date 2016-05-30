3d1104a963bdf6bf55014cfab1c9b319aa3fe6ff
David Wood <david@davidtw.co>
2016-05-30 14:45:15 +0100

    Changed layout.

--- app/forms.py
+++ app/forms.py
@@ -4,11 +4,11 @@ from wtforms.validators import DataRequired, Length
 
 class CommandForm(Form):
     command_name = StringField(label='Command',
-                               description='What command should the user run to get your response?',
+                               description='What command will the user run?',
                                validators=[DataRequired(), Length(min=1, max=20)])
     help_text = TextAreaField(label='Help Text',
-                              description='What should be written next to the command in the `!help` listing?',
+                              description='What does your command do?',
                               validators=[DataRequired()])
     response = TextAreaField(label='Response',
-                             description='What should the bot respond with when the user has run the command?',
+                             description='What should the bot say back?',
                              validators=[DataRequired()])
