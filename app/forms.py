6035ca8bbe004a127fe57c92ad63987f2f96d505
David Wood <david@davidtw.co>
2016-05-30 13:37:26 +0100

    Added command length validator.

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import Form
 from wtforms import StringField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 class CommandForm(Form):
     command_name = StringField(label='Command',
                                description='What command should the user run to get your response?',
-                               validators=[DataRequired()])
+                               validators=[DataRequired(), Length(min=1, max=20)])
     help_text = TextAreaField(label='Help Text',
                               description='What should be written next to the command in the `!help` listing?',
                               validators=[DataRequired()])
